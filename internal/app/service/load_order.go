e3cd2e3cf400689874f8b0ff6db71cc79607ed86
alex0ps <alexey.petrunev@yandex.ru>
2024-04-05 03:42:50 +0300

    fix

--- internal/app/service/load_order.go
+++ internal/app/service/load_order.go
@@ -17,6 +17,43 @@ import (
 	"github.com/ShiraazMoollatjie/goluhn"
 )
 
+func (s *Service) accrualDiscount(ctx context.Context, inputCh <-chan *storage.Order, resultCh chan<- *storage.Order) {
+	for order := range inputCh {
+		client := &http.Client{}
+		// Calculate discount
+		req, err := http.NewRequestWithContext(ctx, "GET", s.AccrualHTTP()+"/api/orders/"+order.Order, nil)
+		if err != nil {
+			s.Logger.Errorln(err.Error())
+			resultCh <- order
+			return
+		}
+		res, err := client.Do(req)
+		if err != nil {
+			s.Logger.Errorln(err.Error())
+			resultCh <- order
+			return
+		}
+		defer res.Body.Close()
+		buf, err := io.ReadAll(res.Body)
+		if err != nil {
+			s.Logger.Errorln(err.Error())
+			resultCh <- order
+			return
+		}
+		// Parse discount
+		if len(buf) > 0 {
+			// change original object
+			if err := json.Unmarshal(buf, &order); err != nil {
+				s.Logger.Errorln(err.Error())
+				resultCh <- order
+				return
+			}
+		}
+		// return
+		resultCh <- order
+	}
+}
+
 func (s *Service) LoadOrder(w http.ResponseWriter, r *http.Request) {
 	// Validate user
 	if err := mycookie.ValidateCookie(r); err != nil {
@@ -67,31 +104,19 @@ func (s *Service) LoadOrder(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	go func() {
-		res, err := http.Get(s.AccrualHTTP() + "/api/orders/" + order.Order)
-		if err != nil {
-			s.Logger.Errorln(err.Error())
-			return
-		}
-		defer res.Body.Close()
-		buf, err := io.ReadAll(res.Body)
-		if err != nil {
-			s.Logger.Errorln(err.Error())
-			return
-		}
-		if len(buf) > 0 {
-			if err := json.Unmarshal(buf, &order); err != nil {
-				s.Logger.Errorln(err.Error())
-				return
-			}
-		}
-		// Update balance
-		ctx := context.Background()
-		err = s.DB.BalanceIncrease(ctx, &user, &order)
-		if err != nil {
-			s.Logger.Errorln(err.Error())
-		}
-	}()
+	inputCh := make(chan *storage.Order, 1)
+	resultCh := make(chan *storage.Order, 1)
+	go s.accrualDiscount(r.Context(), inputCh, resultCh)
+	inputCh <- &order
+	close(inputCh)
+	<-resultCh
+	// Update balance
+	err = s.DB.BalanceIncrease(r.Context(), &user, &order)
+	if err != nil {
+		s.Logger.Errorln(err.Error())
+		myhttp.WriteError(&w, http.StatusInternalServerError, err)
+		return
+	}
 	// 202
 	myhttp.WriteResponse(&w, myhttp.CTypeNone, http.StatusAccepted, nil)
 }
